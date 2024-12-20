3a6272c7f32087458a8ed87b24a8ad311a646053
ravikant yadav <ravikantyadav435@gmail.com>
2024-12-20 22:04:36 +0530

    fixing build failed

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -8,7 +8,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { redirect } from "next/navigation";
 import { getAllNotes, getNotes } from "@/services/actions/notes";
-import ProfilePage from "./_note";
+import ProfilePage from "./_note/_note";
 import { get_user } from "@/services/actions/user-action";
 import Link from "next/link";
 
