89d3890f93c625150547059cb2119a20345944c3
ravikant yadav <ravikantyadav435@gmail.com>
2024-12-30 01:16:08 +0530

    refactoring v1

--- app/page.tsx
+++ app/page.tsx
@@ -22,11 +22,7 @@ export default async function Page() {
         <ModeToggle />
       </div>
       <FloatingNav navItems={[
-        {
-          name: "Dashboard",
-          link: "/dashboard",
-          icon: <LayoutDashboard className="w-5 h-5" />
-        },
+      
         {
           name: "Create Note",
           link: "/note/create",
