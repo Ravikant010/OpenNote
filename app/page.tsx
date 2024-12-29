cd0b8e7b75396598018adbaa3d8e8e7c4a36963c
ravikant yadav <ravikantyadav435@gmail.com>
2024-12-30 00:39:03 +0530

    refactoring v1

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default async function Page() {
         <p className="text-gray-400 text-lg">Discover and explore notes from our community</p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {notes.map((note) => (
+        {notes && notes.map((note) => (
           <Link 
             href={`/${note.userId}/note/${note.id}`} 
             key={note.id}
