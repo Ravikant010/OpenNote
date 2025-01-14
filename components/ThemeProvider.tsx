b2a38f47bb0ba8c79b5c49c0b044af502c8ad752
ravikant yadav <ravikantyadav435@gmail.com>
2025-01-15 03:27:25 +0530

    rewriting and fixing

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -17,7 +17,7 @@ export default function ThemeProvider({ children }: { children: React.ReactNode 
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
 
       enableSystem
       disableTransitionOnChange
